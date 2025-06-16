da27ee0a6b7da88ec858e12eed3bcc29d5086b06
Abdallah Ghamry <125407245+abdallared@users.noreply.github.com>
2025-06-16 10:24:11 +0300

    V4

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ def api_predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    Ensure templates directory exists
+    #Ensure templates directory exists
     if not os.path.exists('templates'):
         os.makedirs('templates')
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    #app.run(debug=True, host='0.0.0.0', port=5000)
